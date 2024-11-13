06be00b6145544e5570034ed36767b6a42cc39d8
NicolaMPM <rrjjkkzz418@gmail.com.com>
2024-11-13 15:17:35 +0300

    ервый коммит

---
+++ src/index2.js
@@ -0,0 +1,6 @@
+import "./App2.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App2 from './App2';
+import './index2.css';
+import reportWebVitals from './reportWebVitals';
